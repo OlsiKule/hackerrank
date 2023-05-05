54df9417033f904b9801f51425461730b3375555
Admin <admin.push@hackerrank.workspace>
2023-05-05 15:31:07 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- app.js
+++ app.js
@@ -33,4 +33,4 @@ app.post('/insert', (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+// app.listen(3000, () => console.log('Server started on port 3000'));
