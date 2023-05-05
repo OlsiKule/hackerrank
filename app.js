f417da6b35e33379ea59ef3fb521668639ef073f
Admin <admin.push@hackerrank.workspace>
2023-05-05 14:56:56 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-var express = require('express');
-var path = require('path');
+// var express = require('express');
+// var path = require('path');
 
-var app = express();
+// var app = express();
 
-app.use(express.static(path.join(__dirname, 'view')));
+// app.use(express.static(path.join(__dirname, 'view')));
 
-app.listen(8000, '0.0.0.0', function() {});
+// app.listen(8000, '0.0.0.0', function() {});
 
-module.exports = app;
+// module.exports = app;
+
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+let list = [];
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/insert', (req, res) => {
+    let value = req.body.value;
+    if (value === '') {
+        res.send('Please provide a valid input');
+    } else {
+        list.push(value);
+        res.send('Success');
+    }
+});
+
+app.listen(3000, () => console.log('Server started on port 3000'));
