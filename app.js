5a85c325687cd51bf146ac6cdcc9e257a9ed67c3
Admin <admin.push@hackerrank.workspace>
2023-05-05 15:36:34 +0000

    Auto save changes before Switch to Local IDE
    Commit by HackerRank

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-// var express = require('express');
-// var path = require('path');
-
-// var app = express();
-
-// app.use(express.static(path.join(__dirname, 'view')));
-
-// app.listen(8000, '0.0.0.0', function() {});
-
-// module.exports = app;
-
-
 const express = require('express');
 const app = express();
 const path = require('path');
